1624a948513ce37e81b5325c8b37639893a80da8
szz110 <szz110@vip.qq.com>
2017-12-09 18:36:06 +0800

    初始化

---
+++ app/src/main/java/cloud/parts/com/parts/PartsApp.java
@@ -0,0 +1,45 @@
+package cloud.parts.com.parts;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.CookieJarImpl;
+import com.lzy.okgo.cookie.store.MemoryCookieStore;
+import com.lzy.okgo.https.HttpsUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by admin on 2017/12/1.
+ */
+
+public class PartsApp extends Application {
+    private static final PartsApp ourInstance = new PartsApp();
+
+    public static PartsApp getInstance() {
+        return ourInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //全局的读取超时时间
+        builder.readTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
+        //全局的写入超时时间
+        builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
+        //全局的连接超时时间
+        builder.connectTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
+        //使用内存保持cookie，app退出后，cookie消失
+        builder.cookieJar(new CookieJarImpl(new MemoryCookieStore()));
+        OkGo.getInstance().init(this)                           //必须调用初始化
+                .setOkHttpClient(builder.build())               //建议设置OkHttpClient，不设置将使用默认的
+                .setCacheMode(CacheMode.NO_CACHE)               //全局统一缓存模式，默认不使用缓存，可以不传
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
+                .setRetryCount(3);                              //全局统一超时重连次数，默认为三次，那么最差的情况会请求4次
+    }
+}
---
+++ app/src/main/java/cloud/parts/com/parts/url/CarUrl.java
@@ -0,0 +1,11 @@
+package cloud.parts.com.parts.url;
+
+/**
+ * 类用途：
+ * 作者：Zhuang
+ * 时间：2017/12/9 13:04
+ */
+
+public class CarUrl {
+    public static final String CAR_URL="http://47.95.240.83:8080/sscvapi/partscloudapi/";
+}
