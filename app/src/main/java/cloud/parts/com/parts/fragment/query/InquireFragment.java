297ed7dcc27be360b521cb9d1b76b45287807bd9
史壮壮 <zhuangzhuang.shi@kangso.net>
2018-04-08 15:12:37 +0800

    整理接口

--- app/src/main/java/cloud/parts/com/parts/fragment/query/InquireFragment.java
+++ app/src/main/java/cloud/parts/com/parts/fragment/query/InquireFragment.java
@@ -53,14 +53,98 @@ import cloud.parts.com.parts.url.urlbean.UrlBean;
  */
 
 public class InquireFragment extends Fragment implements OnClickListener {
+    //行驶证需要的
+    private static final int REQUEST_CODE_VEHICLE_LICENSE = 120;
     private ImageView include_banck;
     private TextView include_titles;
     private ImageView iv_home_scancode;
     private RecyclerView rl_home_carlist;
-    //行驶证需要的
-    private static final int REQUEST_CODE_VEHICLE_LICENSE = 120;
     private EditText et_home_vinnumber;
     private InquireAdapter mAdapter;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_home_scancode:
+                alertShow();
+                InputMethodManager imm = (InputMethodManager)
+                        getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                break;
+            case R.id.include_banck:
+                getActivity().finish();
+                break;
+        }
+    }
+
+    //对话框
+    public void alertShow() {
+        new AlertView("选择搜索方式", null, "取消", null,
+                new String[]{"扫描行驶证", "直接搜索"},
+                getActivity(), AlertView.Style.ActionSheet, new OnItemClickListener() {
+            @Override
+            public void onItemClick(Object o, int position) {
+                if (position == 0) {
+                    if (!PartsApp.hasGotToken) {
+                        return;
+                    }
+                    Intent intent = new Intent(getActivity(), CameraActivity.class);
+                    intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH,
+                            FileUtil.getSaveFile(getActivity().getApplication()).getAbsolutePath());
+                    intent.putExtra(CameraActivity.KEY_CONTENT_TYPE,
+                            CameraActivity.CONTENT_TYPE_GENERAL);
+                    startActivityForResult(intent, REQUEST_CODE_VEHICLE_LICENSE);
+                } else if (position == 1) {
+                    submit();
+                }
+            }
+        }).show();
+    }
+
+    private void submit() {
+        // validate
+        String vinnumber = et_home_vinnumber.getText().toString().trim();
+        if (TextUtils.isEmpty(vinnumber)) {
+            Toast.makeText(getContext(), "请输入VIN码...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(getActivity(), DetailsActivity.class);
+        intent.putExtra("VIN", vinnumber);//vinnumber
+        startActivity(intent);
+        et_home_vinnumber.setText("");
+    }
+
+    /**
+     * OCR识别返回的结果
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 识别成功回调，行驶证识别
+        if (requestCode == REQUEST_CODE_VEHICLE_LICENSE && resultCode == Activity.RESULT_OK) {
+            RecognizeService.recVehicleLicense(FileUtil.getSaveFile(getActivity()
+                            .getApplicationContext()).getAbsolutePath(),
+                    new RecognizeService.ServiceListener() {
+                        @Override
+                        public void onResult(String result) {
+                            //todo ocr返回数据
+                       /*     Toast.makeText(getActivity(), result.toString(), Toast.LENGTH_SHORT)
+                                    .show();*/
+                            Gson gson = new Gson();
+                            VINBean vinBean = gson.fromJson(result.toString(), VINBean.class);
+                            VINBean.WordsResultBean words_result = vinBean.getWords_result();
+                            Intent intent = new Intent(getActivity(), DetailsActivity.class);
+                            intent.putExtra("VIN", words_result.get车辆识别代号().getWords());
+                            Logger.e(words_result.get车辆识别代号().getWords() + "===========vin==============");
+                            startActivity(intent);
+                        }
+                    });
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -75,21 +159,6 @@ public class InquireFragment extends Fragment implements OnClickListener {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initData();
-    }
-
-    private void initView(View rootView) {
-        include_banck = (ImageView) rootView.findViewById(R.id.include_banck);
-        include_banck.setVisibility(View.VISIBLE);
-        include_banck.setOnClickListener(this);
-        include_titles = (TextView) rootView.findViewById(R.id.include_titles);
-        include_titles.setVisibility(View.VISIBLE);
-        include_titles.setText("云配在线");
-        iv_home_scancode = (ImageView) rootView.findViewById(R.id.iv_home_scancode);
-        iv_home_scancode.setOnClickListener(this);
-        rl_home_carlist = (RecyclerView) rootView.findViewById(R.id.rl_home_carlist);
-        rl_home_carlist.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        et_home_vinnumber = (EditText) rootView.findViewById(R.id.et_home_vinnumber);
     }
 
     private void initData() {
@@ -157,77 +226,6 @@ public class InquireFragment extends Fragment implements OnClickListener {
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_home_scancode:
-                alertShow();
-                InputMethodManager imm = (InputMethodManager)
-                        getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                break;
-            case R.id.include_banck:
-                getActivity().finish();
-                break;
-        }
-    }
-
-    //对话框
-    public void alertShow() {
-        new AlertView("选择搜索方式", null, "取消", null,
-                new String[]{"扫描行驶证", "直接搜索"},
-                getActivity(), AlertView.Style.ActionSheet, new OnItemClickListener() {
-            @Override
-            public void onItemClick(Object o, int position) {
-                if (position == 0) {
-                    if (!PartsApp.hasGotToken) {
-                        return;
-                    }
-                    Intent intent = new Intent(getActivity(), CameraActivity.class);
-                    intent.putExtra(CameraActivity.KEY_OUTPUT_FILE_PATH,
-                            FileUtil.getSaveFile(getActivity().getApplication()).getAbsolutePath());
-                    intent.putExtra(CameraActivity.KEY_CONTENT_TYPE,
-                            CameraActivity.CONTENT_TYPE_GENERAL);
-                    startActivityForResult(intent, REQUEST_CODE_VEHICLE_LICENSE);
-                } else if (position == 1) {
-                    submit();
-                }
-            }
-        }).show();
-    }
-
-    /**
-     * OCR识别返回的结果
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // 识别成功回调，行驶证识别
-        if (requestCode == REQUEST_CODE_VEHICLE_LICENSE && resultCode == Activity.RESULT_OK) {
-            RecognizeService.recVehicleLicense(FileUtil.getSaveFile(getActivity()
-                            .getApplicationContext()).getAbsolutePath(),
-                    new RecognizeService.ServiceListener() {
-                        @Override
-                        public void onResult(String result) {
-                            //todo ocr返回数据
-                       /*     Toast.makeText(getActivity(), result.toString(), Toast.LENGTH_SHORT)
-                                    .show();*/
-                            Gson gson = new Gson();
-                            VINBean vinBean = gson.fromJson(result.toString(), VINBean.class);
-                            VINBean.WordsResultBean words_result = vinBean.getWords_result();
-                            Intent intent = new Intent(getActivity(), DetailsActivity.class);
-                            intent.putExtra("VIN",words_result.get车辆识别代号().getWords());
-                            Logger.e(words_result.get车辆识别代号().getWords()+"===========vin==============");
-                            startActivity(intent);
-                        }
-                    });
-        }
-    }
-
-    @Override
     public void onDestroy() {
         super.onDestroy();
         // 释放内存资源
@@ -236,17 +234,18 @@ public class InquireFragment extends Fragment implements OnClickListener {
         }
     }
 
+    private void initView(View rootView) {
+        include_banck = (ImageView) rootView.findViewById(R.id.include_banck);
+        include_banck.setVisibility(View.VISIBLE);
+        include_banck.setOnClickListener(this);
+        include_titles = (TextView) rootView.findViewById(R.id.include_titles);
+        include_titles.setVisibility(View.VISIBLE);
+        include_titles.setText("云配在线");
+        iv_home_scancode = (ImageView) rootView.findViewById(R.id.iv_home_scancode);
+        iv_home_scancode.setOnClickListener(this);
+        rl_home_carlist = (RecyclerView) rootView.findViewById(R.id.rl_home_carlist);
+        rl_home_carlist.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-    private void submit() {
-        // validate
-        String vinnumber = et_home_vinnumber.getText().toString().trim();
-        if (TextUtils.isEmpty(vinnumber)) {
-            Toast.makeText(getContext(), "请输入VIN码...", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Intent intent = new Intent(getActivity(), DetailsActivity.class);
-        intent.putExtra("VIN", vinnumber);//vinnumber
-        startActivity(intent);
-        et_home_vinnumber.setText("");
+        et_home_vinnumber = (EditText) rootView.findViewById(R.id.et_home_vinnumber);
     }
 }
